e2436f01a368668fa004e604dd60539787fe6872
fzx-h <fengzhaoxiang@kylinos.cn>
2020-07-13 16:57:16 +0800

    update debian/copyright files

--- ukui-volume-control-applet-qt/ukmedia_monitor_window_thread.h
+++ ukui-volume-control-applet-qt/ukmedia_monitor_window_thread.h
@@ -1,3 +1,20 @@
+/*
+ * Copyright 2020. kylinos.cn.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, see <http://www.gnu.org/licenses/&gt;.
+ *
+ */
 #ifndef UKMEDIAMONITORWINDOWTHREAD_H
 #define UKMEDIAMONITORWINDOWTHREAD_H
 
@@ -16,6 +33,8 @@
 #include <stdio.h>
 #include <X11/Xatom.h>
 
+//#include <libwnck/libwnck.h>
+
 class UkmediaMonitorWindowThread : public QThread
 {
 public:
@@ -23,7 +42,6 @@ public:
     void get_window_nameAndid();
     //结束线程标志位
     bool bStopThread;
-
     void run();
 };
 
