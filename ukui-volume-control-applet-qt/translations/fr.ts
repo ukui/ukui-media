52e3172c3b030dfb83c6cab8a2fdfb45483bb689
roxfr <roxfr@outlook.fr>
2020-12-12 17:01:17 +0000

    Translated using Weblate (French)
    Currently translated at 100.0% (17 of 17 strings)
    
    Translation: ukui-media/ukui-volume-control
    Translate-URL: https://weblate.ubuntukylin.com/projects/ukui-media/ukui-volume-control/fr/

--- ukui-volume-control-applet-qt/translations/fr.ts
+++ ukui-volume-control-applet-qt/translations/fr.ts
@@ -6,12 +6,12 @@
     <message>
         <location filename="../ukmedia_application_volume_widget.cpp" line="28"/>
         <source>Application Volume</source>
-        <translation type="unfinished"></translation>
+        <translation>Volume d&apos;application</translation>
     </message>
     <message>
         <location filename="../ukmedia_application_volume_widget.cpp" line="30"/>
         <source>System Volume</source>
-        <translation type="unfinished"></translation>
+        <translation>Volume du système</translation>
     </message>
 </context>
 <context>
@@ -19,34 +19,34 @@
     <message>
         <location filename="../ukmedia_device_switch_widget.cpp" line="213"/>
         <source>Go Into Mini Mode</source>
-        <translation type="unfinished"></translation>
+        <translation>Passer en mode mini</translation>
     </message>
     <message>
         <location filename="../ukmedia_device_switch_widget.cpp" line="167"/>
         <source>Output volume control</source>
-        <translation type="unfinished"></translation>
+        <translation>Contrôle du volume de sortie</translation>
     </message>
     <message>
         <location filename="../ukmedia_device_switch_widget.cpp" line="169"/>
         <location filename="../ukmedia_device_switch_widget.cpp" line="172"/>
         <source>Mute</source>
-        <translation type="unfinished"></translation>
+        <translation>Muet</translation>
     </message>
     <message>
         <location filename="../ukmedia_device_switch_widget.cpp" line="170"/>
         <location filename="../ukmedia_device_switch_widget.cpp" line="173"/>
         <source>Sound preference(S)</source>
-        <translation type="unfinished"></translation>
+        <translation>Préférence sonore(S)</translation>
     </message>
     <message>
         <location filename="../ukmedia_device_switch_widget.cpp" line="1234"/>
         <source>Device Volume</source>
-        <translation type="unfinished"></translation>
+        <translation>Volume du périphérique</translation>
     </message>
     <message>
         <location filename="../ukmedia_device_switch_widget.cpp" line="1235"/>
         <source>Application Volume</source>
-        <translation type="unfinished"></translation>
+        <translation>Volume d&apos;application</translation>
     </message>
     <message>
         <location filename="../ukmedia_device_switch_widget.cpp" line="2495"/>
@@ -54,13 +54,13 @@
         <location filename="../ukmedia_device_switch_widget.cpp" line="2757"/>
         <location filename="../ukmedia_device_switch_widget.cpp" line="2765"/>
         <source>Speaker (Realtek Audio)</source>
-        <translation type="unfinished"></translation>
+        <translation>Haut-parleur (Realtek Audio)</translation>
     </message>
     <message>
         <location filename="../ukmedia_device_switch_widget.cpp" line="2498"/>
         <location filename="../ukmedia_device_switch_widget.cpp" line="2761"/>
         <source>Headphone</source>
-        <translation type="unfinished"></translation>
+        <translation>Casque</translation>
     </message>
 </context>
 <context>
@@ -68,27 +68,27 @@
     <message>
         <location filename="../ukmedia_device_volume_widget.cpp" line="36"/>
         <source>Input Device</source>
-        <translation type="unfinished"></translation>
+        <translation>Périphérique d&apos;entrée</translation>
     </message>
     <message>
         <location filename="../ukmedia_device_volume_widget.cpp" line="37"/>
         <source>Microphone</source>
-        <translation type="unfinished"></translation>
+        <translation>Microphone</translation>
     </message>
     <message>
         <location filename="../ukmedia_device_volume_widget.cpp" line="45"/>
         <source>Output Device</source>
-        <translation type="unfinished"></translation>
+        <translation>Périphérique de sortie</translation>
     </message>
     <message>
         <location filename="../ukmedia_device_volume_widget.cpp" line="46"/>
         <source>Speaker Realtek Audio</source>
-        <translation type="unfinished"></translation>
+        <translation>Haut-parleur Realtek Audio</translation>
     </message>
     <message>
         <location filename="../ukmedia_device_volume_widget.cpp" line="49"/>
         <source>Input device can not be detected</source>
-        <translation type="unfinished"></translation>
+        <translation>Le périphérique d&apos;entrée ne peut pas être détecté</translation>
     </message>
 </context>
 <context>
@@ -96,12 +96,12 @@
     <message>
         <location filename="../ukmedia_mini_master_volume_widget.cpp" line="48"/>
         <source>Speaker (Realtek Audio)</source>
-        <translation type="unfinished"></translation>
+        <translation>Haut-parleur (Realtek Audio)</translation>
     </message>
     <message>
         <location filename="../ukmedia_mini_master_volume_widget.cpp" line="116"/>
         <source>Go Into Full Mode</source>
-        <translation type="unfinished"></translation>
+        <translation>Passer en mode complet</translation>
     </message>
 </context>
 </TS>
