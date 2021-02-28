daa9851f4b31b17b2eda7bb241c4539589c9fdbf
Hugo Carvalho <hugokarvalho@hotmail.com>
2021-02-28 15:35:24 +0000

    Added translation using Weblate (Portuguese)

---
+++ ukui-volume-control-applet-qt/translations/pt.ts
@@ -0,0 +1,110 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="pt">
+<context>
+    <name>ApplicationVolumeWidget</name>
+    <message>
+        <location filename="../ukmedia_application_volume_widget.cpp" line="28"/>
+        <source>Application Volume</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ukmedia_application_volume_widget.cpp" line="30"/>
+        <source>System Volume</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>DeviceSwitchWidget</name>
+    <message>
+        <location filename="../ukmedia_device_switch_widget.cpp" line="276"/>
+        <source>Go Into Mini Mode</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ukmedia_device_switch_widget.cpp" line="232"/>
+        <source>Output volume control</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ukmedia_device_switch_widget.cpp" line="234"/>
+        <location filename="../ukmedia_device_switch_widget.cpp" line="237"/>
+        <source>Mute</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ukmedia_device_switch_widget.cpp" line="235"/>
+        <location filename="../ukmedia_device_switch_widget.cpp" line="238"/>
+        <source>Sound preference(S)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ukmedia_device_switch_widget.cpp" line="1284"/>
+        <source>Device Volume</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ukmedia_device_switch_widget.cpp" line="1285"/>
+        <source>Application Volume</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ukmedia_device_switch_widget.cpp" line="2279"/>
+        <source>Error</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ukmedia_device_switch_widget.cpp" line="2279"/>
+        <source>Unable to connect to the sound system, please check whether the pulseaudio service is running!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ukmedia_device_switch_widget.cpp" line="2450"/>
+        <location filename="../ukmedia_device_switch_widget.cpp" line="2451"/>
+        <location filename="../ukmedia_device_switch_widget.cpp" line="2519"/>
+        <location filename="../ukmedia_device_switch_widget.cpp" line="2520"/>
+        <source>Bluetooth</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ukmedia_device_switch_widget.cpp" line="2500"/>
+        <location filename="../ukmedia_device_switch_widget.cpp" line="2501"/>
+        <source>Dummy output</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../ukmedia_device_switch_widget.cpp" line="165"/>
+        <source>PulseAudio Volume Control</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>UkmediaDeviceWidget</name>
+    <message>
+        <location filename="../ukmedia_device_volume_widget.cpp" line="36"/>
+        <source>Input Device</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ukmedia_device_volume_widget.cpp" line="45"/>
+        <source>Output Device</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ukmedia_device_volume_widget.cpp" line="49"/>
+        <source>Input device can not be detected</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>UkmediaMiniMasterVolumeWidget</name>
+    <message>
+        <location filename="../ukmedia_mini_master_volume_widget.cpp" line="114"/>
+        <source>Go Into Full Mode</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
